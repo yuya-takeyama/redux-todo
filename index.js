db262908549e61aa13b18f0ce22f21d59589f76d
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-06-27 00:52:36 +0900

    Fix deploy

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
-var config = require('./webpack.config')
-
 var app = new (require('express'))()
 var port = process.env.PORT || 8081
 
-var compiler = webpack(config)
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
-app.use(webpackHotMiddleware(compiler))
+if (process.env.NODE_ENV !== 'production') {
+  var webpack = require('webpack')
+  var webpackDevMiddleware = require('webpack-dev-middleware')
+  var webpackHotMiddleware = require('webpack-hot-middleware')
+  var config = require('./webpack.config')
+
+  var compiler = webpack(config)
+  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+  app.use(webpackHotMiddleware(compiler));
+}
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + '/public/index.html')

930572f1d5cb9d334438c73a9a9a964b10ed24d3
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-07-01 02:31:12 +0900

    More refactoring

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
 
-export default class Todo extends React.Component {
-  onToggleTodo() {
-    const { store } = this.context;
+let Todo = ({ title, completed, onClick }) => (
+  <li
+    style={{textDecoration: completed ? 'line-through' : 'none'}}
+    onClick={onClick}
+    >
+    {title}
+  </li>
+);
 
-    store.dispatch({type: 'TOGGLE_TODO', id: this.props.id});
-  }
-
-  render() {
-    const { id, title, completed } = this.props;
-
-    return (
-      <li
-        onClick={this.onToggleTodo.bind(this)}
-        key={id}
-        style={{textDecoration: completed ? 'line-through' : 'none'}}
-        >
-        {id}: {title}
-      </li>
-    );
-  }
-}
-
-Todo.contextTypes = {
-  store: React.PropTypes.object
-};
+export default Todo;

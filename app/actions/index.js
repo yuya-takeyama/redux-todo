6f957cbfe32b8a16e39796ce3d3d13ec3e67ba8f
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-07-01 02:34:08 +0900

    lint

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,15 +1,11 @@
 let nextTodoId = 0;
-export const addTodo = (title) => {
-  return {
-    type: 'ADD_TODO',
-    id: nextTodoId++,
-    title,
-  };
-};
+export const addTodo = (title) => ({
+  type: 'ADD_TODO',
+  id: nextTodoId++,
+  title,
+});
 
-export const toggleTodo = (id) => {
-  return {
-    type: 'TOGGLE_TODO',
-    id,
-  };
-};
+export const toggleTodo = (id) => ({
+  type: 'TOGGLE_TODO',
+  id,
+});

1142da758d7fc338fea77b0fcac2a6138808484b
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-07-02 21:44:07 +0900

    Now VisibleTodoList does not require props

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -4,23 +4,10 @@ import VisibleTodoList from '../containers/VisibleTodoList';
 import VisiblityFilter from './VisiblityFilter';
 
 export default class TodoApp extends React.Component {
-  getVisibleTodos() {
-    const { todos, visiblityFilter } = this.props;
-
-    switch (visiblityFilter) {
-      case 'Incomplete':
-        return todos.filter(todo => !todo.completed);
-      case 'Completed':
-        return todos.filter(todo => todo.completed);
-      default:
-        return todos;
-    }
-  }
-
   render() {
     return (
       <div>
-        <VisibleTodoList todos={this.getVisibleTodos()} />
+        <VisibleTodoList />
         <VisiblityFilter visiblityFilter={this.props.visiblityFilter}/>
       </div>
     );
